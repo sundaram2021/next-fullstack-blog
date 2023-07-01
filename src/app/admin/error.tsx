1fb8ded165688daa158d767a5961dea4f40a566a
Sundaram Kumar Jha <jhasundarm@gmail.com>
2023-07-01 20:30:37 +0530

    added some changes

--- src/app/admin/error.tsx
+++ src/app/admin/error.tsx
@@ -16,10 +16,10 @@ export default function Error({
  
   return (
     <div className='w-[100vw] h-[100vh] flex justify-center items-center'>
-      <div className='shadow-xl bg-slate-400'>
+      <div className='shadow-xl bg-slate-400 h-20 rounded-md'>
         <h2 className='font-medium'>Something went wrong in Admin Permissions</h2>
         <button
-            className='bg-slate-500 hover:bg-slate-600 text-white font-bold py-2 px-4 rounded'
+            className='bg-slate-500 hover:bg-slate-600 text-white font-bold py-2 px-4 rounded mx-auto'
             onClick={
             // Attempt to recover by trying to re-render the segment
             () => reset()
--- src/app/settings/error.tsx
+++ src/app/settings/error.tsx
@@ -17,7 +17,7 @@ export default function Error({
   return (
     <div className='w-[100vw] h-[100vh] flex justify-center items-center'>
       <div className='shadow-xl bg-slate-400 h-20 rounded-md'>
-        <h2 className='font-medium'>Something went wrong in Fetching Blogs</h2>
+        <h2 className='font-medium'>Something went wrong in Settings</h2>
         <button
             className='bg-slate-500 hover:bg-slate-600 text-white font-bold py-2 px-4 rounded mx-auto'
             onClick={
